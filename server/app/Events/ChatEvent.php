aebe4aa43e8e1df2a1c6964f5b23e421637115e6
Rajan Kumar S <rks9025676@gmail.com>
2021-03-17 22:53:42 +0530

    Chat Started

---
+++ server/app/Events/ChatEvent.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Events;
+
+use App\Chat;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class ChatEvent implements ShouldBroadcast
+{
+    use Dispatchable;
+    use InteractsWithSockets;
+    use SerializesModels;
+
+    public $chat;
+
+    public function __construct(Chat $chat)
+    {
+        $this->chat = $chat;
+    }
+
+    public function broadcastOn()
+    {
+        return new PresenceChannel('chat');
+    }
+}
--- server/routes/web.php
+++ server/routes/web.php
@@ -16,3 +16,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/chats', 'ChatController@index')->middleware('cors');
+Route::get('/messages', 'ChatController@fetchAllMessages')->middleware('cors');
+Route::post('/messages', 'ChatController@sendMessage')->middleware('cors');
